12de5e161f343eddf370792c4d5f6959e4343f5e
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-03 17:19:30 -0300

    criando o radio com endereço

--- slave/slave.ino
+++ slave/slave.ino
@@ -2,6 +2,8 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+RF24 radio(9,10); //pinos (CE,CSN)
+const byte endereco[6] = "00001"// endereço unico por projeto
 void setup() {
   // put your setup code here, to run once:
 
