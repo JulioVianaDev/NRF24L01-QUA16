69839568be100945a4cbed0e313c1b3ef16136a1
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-10 17:09:39 -0300

    criei o radio e as variaveis

--- master/master/master.ino
+++ master/master/master.ino
@@ -2,6 +2,13 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+RF24 radio(9,10);//criando o radio
+const byte endereco[6] = "00001"; //cria um endereço 
+//MESMO ENDEREÇO DO SLAVE
+
+int pinoBotao = 2; //pino do botao no arduino
+int estadoBotao = 0; //variavel para armazenar o estado
+
 void setup() {
   // put your setup code here, to run once:
 
